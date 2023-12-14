aad92b08c0d003730519a4792b134669ac05785d
JacobYankee <jmyankee3@gmail.com>
2023-12-13 19:47:56 -0500

    Get a full description of a movie review
    In youtube.py: Created a ‘get_video_description’ method that takes a youtube video id as a parameter and returns the full description of the video.
    
    In main.py: Created an API method that uses the ‘get_video_description’ method to output the complete description of the video. It is available in the SwaggerUI.
    
    In test_youtube.py: Created a test method ‘test_get_video_description’ that gets the video description of a valid video (currently with the id ‘U3dE0sYZqvI’). The test passes if the method does not return the response 'Description not found.', which would indicate that the video does not exist. Also created a test method that tests if a video does not exist. It passes if it returns the response 'Description not found.'.
    
    In test_main.py: Created a test method ‘test_video_description’ that gets the video description of a valid video. If it does not return the JSON response {'description': 'Description not found.'}, the test passes. Also created a test method that tests if a video does not exist. It passes if it returns the JSON response {'description': 'Description not found.'}.
    
    Issue #3

--- test_main.py
+++ test_main.py
@@ -7,9 +7,9 @@ from unittest.mock import patch
 client = TestClient(app)
 
 def test_read_main():
-    response = client.get("/")
+    response = client.get('/')
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    assert response.json() == {'message': 'Hello World'}
 
 @pytest.fixture
 def mock_search_movie_reviews(mocker):
@@ -24,8 +24,24 @@ def mock_search_movie_reviews(mocker):
     return mock
 
 def test_get_movie_reviews(mock_search_movie_reviews):
-    movie_name = "test_movie"
-    response = client.get(f"/moviereviews/{movie_name}")
+    movie_name = 'test_movie'
+    response = client.get(f'/moviereviews/{movie_name}')
     assert response.status_code == 200
     assert response.json() == mock_search_movie_reviews.return_value
     mock_search_movie_reviews.assert_called_once_with(movie_name)
+
+def test_video_description():
+    # Get response from valid video id
+    response = client.get('/descriptions/U3dE0sYZqvI')
+
+    # Assert the response status code and content
+    assert response.status_code == 200
+    assert response.json() != {'description': 'Description not found.'}
+
+def test_video_description_no_description():
+    # Get response from valid video id
+    response = client.get('/descriptions/wow')
+
+    # Assert the response status code and content
+    assert response.status_code == 200
+    assert response.json() == {'description': 'Description not found.'}
--- test_youtube.py
+++ test_youtube.py
@@ -1,7 +1,6 @@
 import pytest
-from unittest.mock import Mock
 from googleapiclient.discovery import build
-from youtube import search_movie_reviews
+from youtube import search_movie_reviews, get_video_description
 
 def test_search_movie_reviews(mocker):
     # Mock the build function to return a mock object
@@ -9,12 +8,12 @@ def test_search_movie_reviews(mocker):
 
     # Mock for the YouTube API response with 10 items
     youtube_response = {
-        "items": [
+        'items': [
             {
-                "id": {"videoId": f"video{i}"},
-                "snippet": {
-                    "title": f"Review {i}",
-                    "description": f"Description of review {i}"
+                'id': {'videoId': f'video{i}'},
+                'snippet': {
+                    'title': f'Review {i}',
+                    'description': f'Description of review {i}'
                 }
             } for i in range(10)  # Generating 10 items for the mock response
         ]
@@ -24,7 +23,21 @@ def test_search_movie_reviews(mocker):
     mock_build().search().list().execute.return_value = youtube_response
 
     # Call search_movie_reviews function with a test query
-    results = search_movie_reviews("Harry Potter")
+    results = search_movie_reviews('Harry Potter')
 
     # Assert the expected outcomes from the function
     assert len(results) == 10  # Now it checks if ten results are returned
+
+def test_get_video_description():
+    # Call get_video_description with an actual video_id
+    description = get_video_description('U3dE0sYZqvI')
+
+    # Assert that the description is not empty
+    assert description != 'Description not found.'
+
+def test_get_video_description_no_description():
+    # Call get_video_description with an actual video_id
+    description = get_video_description('wow')
+
+    # Assert that the description is empty
+    assert description == 'Description not found.'
